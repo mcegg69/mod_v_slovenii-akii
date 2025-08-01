e2438073362b345b9bf84635c8ee0a8bd028c254
mcegg69 <122733348+mcegg69@users.noreply.github.com>
2025-08-01 21:27:00 +0300

    UPDATE

--- src/main/java/net/dude/mymod/item/ModItemGroups.java
+++ src/main/java/net/dude/mymod/item/ModItemGroups.java
@@ -14,29 +14,45 @@ import net.minecraft.util.Identifier;
 //привет сладкий
 
 public class ModItemGroups {
-    public static final ItemGroup V_SLOVAKII_GROUP1 = Registry.register(
+    public static final ItemGroup V_SLOVAKII_GROUP_BLOCKS = Registry.register(
             Registries.ITEM_GROUP,
             Identifier.of(Mymod.MOD_ID,"v_slovakii_group"),
-            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.V_SLOVAKII))
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModBlocks.BLOCK_V_SLOVAKII))
                     .displayName(Text.translatable("itemgroup.mymod.v_slovakii_group"))
                     .entries((displayContext, entries) -> {
                         entries.add(ModBlocks.BLOCK_V_SLOVAKII);
                         entries.add(ModBlocks.RAW_V_SLOVAKII_ORE);
                         entries.add(ModBlocks.RAW_V_SLOVAKII_DEEPSLATE_ORE);
+                    }).build());
+
+    public static final ItemGroup V_SLOVAKII_GROUP_ITEMS = Registry.register(
+            Registries.ITEM_GROUP,
+            Identifier.of(Mymod.MOD_ID,"v_slovakii_group"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.V_SLOVAKII))
+                    .displayName(Text.translatable("itemgroup.mymod.v_slovakii_group"))
+                    .entries((displayContext, entries) -> {
                         entries.add(ModItems.V_SLOVAKII);
                         entries.add(ModItems.RAW_V_SLOVAKII);
                         entries.add(ModItems.V_SLOVAKII_MIDAS);
                     }).build());
 
-    public static final ItemGroup V_SLOVENII_GROUP1 = Registry.register(
+    public static final ItemGroup V_SLOVENII_GROUP_BLOCKS = Registry.register(
             Registries.ITEM_GROUP,
             Identifier.of(Mymod.MOD_ID,"v_slovenii_group"),
-            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.V_SLOVENII))
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModBlocks.BLOCK_V_SLOVENII))
                     .displayName(Text.translatable("itemgroup.mymod.v_slovenii_group"))
                     .entries((displayContext, entries) -> {
                         entries.add(ModBlocks.BLOCK_V_SLOVENII);
                         entries.add(ModBlocks.RAW_V_SLOVENII_ORE);
                         entries.add(ModBlocks.RAW_V_SLOVENII_DEEPSLATE_ORE);
+                    }).build());
+
+public static final ItemGroup V_SLOVENII_GROUP_ITEMS = Registry.register(
+            Registries.ITEM_GROUP,
+            Identifier.of(Mymod.MOD_ID,"v_slovenii_group"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.V_SLOVENII))
+                    .displayName(Text.translatable("itemgroup.mymod.v_slovenii_group"))
+                    .entries((displayContext, entries) -> {
                         entries.add(ModItems.V_SLOVENII);
                         entries.add(ModItems.RAW_V_SLOVENII);
                         entries.add(ModItems.V_SLOVENII_MIDAS);
